88b46c5ba21a6b9b8632c85cf7dde60ae2398d90
mariapaula1806 <maria.elias.souza18@escola.pr.gov.br>
2026-03-04 15:40:07 -0300

    esfera

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(1920, 1080, WEBGL);//plano 3D
+    terra=loadImage('terrajpg')
+}
+
+function draw() {
+background(205, 102, 94);//cor de fundo
+rotateY(millis()/1000);//rotação da terra, imagem.
+sphere (80, 100);//esfera 3D
+}
