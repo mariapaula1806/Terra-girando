cbd709044ca873b30ac4aa9a99e2b59097393bf9
mariapaula1806 <maria.elias.souza18@escola.pr.gov.br>
2026-03-11 15:19:44 -0300

    sketch123

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+let terra;
+
+function preload(){
+    terra = loadImage('terra.jpg')
+}
 function setup() {
     createCanvas(1920, 1080, WEBGL);//plano 3D
-    terra=loadImage('terrajpg')
 }
 
 function draw() {
 background(205, 102, 94);//cor de fundo
 rotateY(millis()/1000);//rotação da terra, imagem.
+TextTrackCue(terra);//textura da terra, imagem.
 sphere (80, 100);//esfera 3D
 }
