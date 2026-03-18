2ec83f3f792c08263dd766d9e5d80e21068d1ef4
mariapaula1806 <maria.elias.souza18@escola.pr.gov.br>
2026-03-18 15:12:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
-    createCanvas(1920, 1080, WEBGL);//plano 3D
+    createCanvas(1920,1080, WEBGL);//plano 3D
     terra=loadImage('terra.jpg')
 }
 
 function draw() {
-background(205, 102, 94);//cor de fundo
-rotateY(millis()/1000);//rotação da terra, imagem.
-TextTrackCue(terra);//textura da terra, imagem.
-sphere (80, 100);//esfera 3D
+    background(205, 102, 94);//cor de fundo
+    rotateY(millis()/1000);//rotação da terra
+    texture(terra);//textura da terra, imagem.
+    sphere(80, 100);//esfera 3D
 }
